12c8c6a649750a992a66399fcd8f584c9dbadd77
JuantMartinez17 <juantomasm0@gmail.com>
2024-10-25 21:25:57 -0300

    Fragments transaction implemented

--- app/src/main/java/com/example/fragment/ListFragment.kt
+++ app/src/main/java/com/example/fragment/ListFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import com.example.fragment.databinding.FragmentListBinding
 
 
 class ListFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param2: String? = null
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +26,15 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnFragmentList.setOnClickListener{
+            val detailFragment = DetailFragment()
+            fragmentManager?.beginTransaction()?.replace(R.id.fragment_containerView, detailFragment)?.commit()
+        }
     }
 }
